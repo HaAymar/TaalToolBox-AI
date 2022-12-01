d8c2751826365d3f3b5e2418ba49ed43407ff1a5
Patson <cardin.tiako@gmail.com>
2022-12-01 10:58:55 +0100

    feat(record): record signal and generate graph

---
+++ rec_signal.py
@@ -0,0 +1,121 @@
+
+# import sounddevice as sd
+import pyaudio
+from scipy.io.wavfile import write
+from scipy.io import wavfile
+import wavio as wv
+import parselmouth
+import wave
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+user_sound = 'user_sound.wav'
+#simply frequency
+freq = 44100
+
+
+# time to store data
+duration = 5
+
+
+# Start recorder with the given values of 
+# duration and sample frequency
+
+p = pyaudio.PyAudio()
+
+print('Recording')
+
+stream = p.open(format=pyaudio.paInt16, channels=2, rate=freq, frames_per_buffer=1024, input=True)
+
+frames = [] # Array pour stocker les trames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(freq / 1024 * duration)):
+    data = stream.read(1024)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+# Save the recorded data as a WAV file
+wf = wave.open(user_sound, 'wb')
+wf.setnchannels(2)
+wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+wf.setframerate(freq)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+samplerate, data = wavfile.read(user_sound)
+
+print( "sample rate" , samplerate , "longueur de data : "  , len(data), 'data' , data)
+# recording = sd.rec(int(duration * freq), samplerate=freq,channels=2)
+
+# wf.wait()
+# sd.wait()
+
+# This will convert the NumPy array to an audio
+# file with the given sampling frequency
+# write("recording0.wav", freq, recording)
+  
+# Convert the NumPy array to audio file
+# wv.write("recording1.wav", recording, freq, sampwidth=2)
+
+sns.set()
+
+plt.rcParams['figure.dpi'] = 100 # Show nicely large images in this notebook
+snd = parselmouth.Sound(user_sound)
+
+# snd is all the user sound
+# need to elimate all the unused part of the sound
+# detect when the sound start and finish exactly (the time)
+
+# put all the value of the sound in an array
+snd_part = snd.extract_part(from_time=1.2, preserve_times=True)
+
+window = 4
+
+snd_list = []
+# snd_list.append(snd_part)
+
+# print(snd_list)
+# Object type: Sound
+# Object name: <no name>
+# Date: Wed Nov 30 11:00:42 2022
+
+# Number of channels: 2 (stereo)
+# Time domain:
+#    Start time: 1.2 seconds
+#    End time: 4.992290249433107 seconds
+#    Total duration: 3.7922902494331066 seconds
+# Time sampling:
+#    Number of samples: 167240
+#    Sampling period: 2.2675736961451248e-05 seconds
+#    Sampling frequency: 44100 Hz
+#    First sample centred at: 1.2000113378684807 seconds
+# Amplitude:
+#    Minimum: -0.079864502 Pascal
+#    Maximum: 0.105438232 Pascal
+#    Mean: 1.31748932e-06 Pascal
+#    Root-mean-square: 0.021210851 Pascal
+# Total energy: 0.00170615214 Pascal² sec (energy in air: 4.26538036e-06 Joule/m²)
+# Mean power (intensity) in air: 1.1247505e-06 Watt/m² = 60.51 dB
+# Standard deviation in channel 1: 0.0212109622 Pascal
+# Standard deviation in channel 2: 0.0212108666 Pascal
+
+# for ind in range(len(snd_part) - window+1):
+#     snd_part_average.append(np.mean(snd_part[ind:ind+window]))
+    
+# print(snd_part_average)
+
+plt.figure()
+plt.plot(snd_part.xs(), snd_part.values.T)
+plt.xlim([snd_part.xmin, snd_part.xmax])
+plt.xlabel("time [s]")
+plt.ylabel("amplitude")
+plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
+
+
