aaac17d331d1d2edd10561b2aed999e5be22bedf
Patson <cardin.tiako@gmail.com>
2022-12-05 19:06:47 +0100

    update(moving average) increase the moving average

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from module.speech_recognition.rec_audio import speech_recognition
 from module.signal_traitement.rec_signal import audio_traitement
-# from module.test_signal.signal_test import signal_test
+from module.test_signal.signal_test import signal_test
 
 # valeur (racine carre de la somme de carre / par la s)
 if __name__ == '__main__':
     # Signal Traitement
-    audio_traitement()
+    # audio_traitement()
 
     # Speech Recognition
     # print(speech_recognition("./speech_recognition/test/test-set/set1/rec1.wav", "mijn naam").getPrononciation())
@@ -13,5 +13,5 @@ if __name__ == '__main__':
 
     # Class servant de test pour le signal
 
-    # signal_test()
+    signal_test()
 
--- module/signal_traitement/rec_signal.py
+++ module/signal_traitement/rec_signal.py
@@ -22,9 +22,9 @@ class audio_traitement:
         self.__user_sound = 'user_sound.wav'
         self.audio = pyaudio.PyAudio()
         self.frames = [] # Array pour stocker les trames
-        self.record(self.audio, self.freq , self.__duration, self.frames )
-        data = self.save_record(self.audio, self.freq, self.frames, self.__user_sound)
-        # samplerate, data = wavfile.read(self.__user_sound)
+        # self.record(self.audio, self.freq , self.__duration, self.frames )
+        # data = self.save_record(self.audio, self.freq, self.frames, self.__user_sound)
+        samplerate, data = wavfile.read(self.__user_sound)
         efficace_data = self.efficace_value(data)
         moving_data = self.moving_average_of_2d_array(efficace_data)
         # print(moving_data)
@@ -32,7 +32,8 @@ class audio_traitement:
         rms = self.rms_of_signal(moving_data)
 
 
-        self.generate_graph(self.__user_sound , fft_data)
+        # self.generate_graph(self.__user_sound , fft_data)
+        self.generate_graph(self.__user_sound , moving_data, fft_data)
 
 
     def record(self,audio, freq, duration, frames):
@@ -95,16 +96,59 @@ class audio_traitement:
 
         return efficace_array
 
-
-    def moving_average_of_2d_array(self, array, window=1000):
+        # def moving_average_of_2d_array(self, array, window=3):
+    #     array_average = []
+    #     for ind in range(len(array) - window+1):
+    #         array_average.append(np.mean(array[ind:ind+window]))
+    #     return array_average
+    
+    
+    def moving_average_of_2d_array(self, list_array, window=1001):
         array_average = []
+        array = list(list_array)
         # print(array)
-        for ind in range(len(array) - window+1):
-            array_average.append(np.mean(array[ind:ind+window]))
+        import functools
+        # for pos in range(0, len(array)):
+        #     temp_calc = 0
+        #     for i in range(pos-(window//2), pos+(window//2)+1):
+        #         if (i >= 0) & (i < (len(array))):
+        #             # print(i, len(list))
+        #             temp_calc = temp_calc + array[i]
+        #     array_average.append(temp_calc/window)
+                # actual_ind = np.mean(array[ind:])
+        for ind in range(len(array)):
+            if ind < window//2 :
+                actual_ind = functools.reduce(lambda x, y : x+y , array[ind: ind + (window//2)+1])
+                if ind > 0:
+                    actual_ind = actual_ind + functools.reduce(lambda x, y : x+y , array[0 : ind])
+                average_ind = actual_ind/window
+                array_average.append(average_ind)
+            elif ind >= len(array) - (window//2):
+                actual_ind = functools.reduce(lambda x, y : x+y , array[ind - (window//2) : ind+1 ])
+                if ind < len(array)-1:
+                    actual_ind = actual_ind + functools.reduce(lambda x, y : x+y , array[ind+1:])
+                average_ind = actual_ind/window
+                array_average.append(average_ind)
+                
+            else:
+                actual_ind = functools.reduce(lambda x, y : x+y ,  array[ind - (window//2) : ind]) 
+                actual_ind = actual_ind + functools.reduce(lambda x, y : x+y ,  array[ind: ind+((window//2)+1)]) 
+                average_ind = actual_ind/window
+                array_average.append(average_ind)
+                
+                
+        # return array_average        
+            
+            # array_average.append(np.mean(array[ind:ind+window]))
 
-        # print(f" leng == {len(array_average)}")
+        # [[1,2] , [3,4], [5,6], [7,8] , [9,10], [10,11]]
+        
+        # [[4/3,4] , []]
+        print(f" leng == {len(array_average)}")
         # print(array_average)
         return array_average
+    
+    
     # recording = sd.rec(int(duration * freq), samplerate=freq,channels=2)
 
     # wf.wait()
@@ -116,8 +160,8 @@ class audio_traitement:
 
     # Convert the NumPy array to audio file
     # wv.write("recording1.wav", recording, freq, sampwidth=2)
-    def generate_graph(self , user_sound , data):
-        # print("Bonjour", data)
+    def generate_graph(self , user_sound , moving_data, data):
+        print("Bonjour", data)
         sns.set()
 
         plt.rcParams['figure.dpi'] = 100 # Show nicely large images in this notebook
@@ -166,26 +210,24 @@ class audio_traitement:
         # print(snd_part_average)
 
         plt.figure()
-        plt.subplot(1, 2, 1)
+        plt.subplot(3, 1, 1)
         plt.plot(snd_part.xs(), snd_part.values.T)
         plt.xlim([snd_part.xmin, snd_part.xmax])
         plt.xlabel("time [s]")
         plt.ylabel("amplitude")
         # plt.axis([1.7, 2.5, -0.15 , 0.15])
         # plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
-        plt.subplot(1, 2, 2)
-        # print("data" , len(data))
+        plt.subplot(3, 1, 2)
+        plt.plot(moving_data)
+        plt.subplot(3, 1, 3)
+        print("data" , len(data))
         xf = rfftfreq((len(data)*2)-1, 1 / self.freq)
         # xf = fftfreq(len(data), 1 / self.freq)
         plt.plot(xf , np.abs(data))
 
         plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
 
-    def moving_average_of_2d_array(self, array, window=3):
-        array_average = []
-        for ind in range(len(array) - window+1):
-            array_average.append(np.mean(array[ind:ind+window]))
-        return array_average
+
 
     def fast_fourier_transform(self, array):
         # fft = np.fft.fft(array)
